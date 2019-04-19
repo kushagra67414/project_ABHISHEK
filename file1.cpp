9c2e19d4a96d16b4372b6306c71a3f66a5d1abab
AbhishekYuvi <singhabhishek.7484@gmail.com>
2019-04-19 14:49:03 +0530

    1st

---
+++ file1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+#define FALSE 0
+#define TRUE 1
+using namespace std;
+class linklist
+{
+private:
+	struct node
+	{
+		int data;
+		node* link;
+	}*p;
+public:
+	linklist();
+	void append(int num);
+	void addatbeg(int num);
+	void addafter(int c, int num);
+	void del(int num);
+	void display();
