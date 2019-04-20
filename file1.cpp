b10ab838a7518538857574fd6f177d2dcaef2daa
kushagra67414 <46487696+kushagra67414@users.noreply.github.com>
2019-04-20 16:51:25 +0530

    few lines added

--- file1.cpp
+++ file1.cpp
@@ -133,3 +133,63 @@ int linklist::operator==(linklist t)
                 return flag;
 }
 
+#include<iostream>
+#include<cstring>
+#include<conio.h>
+
+using namespace std;
+
+class String
+{
+protected:
+	char str[200];
+public:
+	String(){}
+	String(char* xstr){ strcpy(str, xstr);	}
+        void show() { cout << str << endl; }
+	char* operator+(String);
+	char* operator=(String);
+	char *operator+=(String);
+        int stringlen();
+        int stringsize();
+	void tolower();
+	void toupper();
+};
+
+char* String::operator+(String xstr)
+{
+	strcat(str, xstr.str);
+	return str;
+}
+char* String::operator+=(String xstr)
+{
+	strcat(str, xstr.str);
+	return str;
+}
+int String::stringsize()
+{
+	return (strlen(str) + 1);
+}
+char* String::operator=(String xstr)
+{
+	strcpy(str, xstr.str);
+	return str;
+}
+int String::stringlen()
+	{
+		return strlen(str);
+	}
+
+
+void String::tolower()
+{
+	for (int i = 0; str[i] != '\0'; i++)
+		if (isupper(str[i]))
+			str[i] += 32;
+}
+void String::toupper()
+{
+	for (int i = 0; str[i] != '\0'; i++)
+		if (islower(str[i]))
+			str[i] -= 32;
+}
